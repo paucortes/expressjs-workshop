0ec628fbd33009725c1a6b443ecf0e2e8510e2d7
Pau <paucortes2@gmail.com>
2016-05-02 19:00:08 +0000

    Completed exercise 1 and 2

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 
+function greet(firstName) {
+  return '<h1>Hello, ' + firstName + '!</h1>';
+}
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/hello', function(request, response) {
+  response.send('<h1>Hello, world!</h1>');
+});
 
-
-
+app.get('/hello/:name', function(request, response) {
+  var firstName = request.params.name;
+  var result = greet(firstName);
+  response.send(result);
+});
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
